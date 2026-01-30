b32542bd592d8da4f41d493fa18cc6bd2a418380
‘joses’ <josesallan@hotmail.com>
2026-01-30 20:02:43 +0100

    Ejercicios actualizados a día 30 de enero de 2026

--- ficheros09.py
+++
@@ -1,8 +0,0 @@
-mates = int(input('Calificación Matemáticas:'))
-lengua = int(input('Calificación Lengua:'))
-historia = int(input('Calificación Historia:'))
-tic = int(input('Calificación TIC II:'))
-notas = [['Matemáticas', mates], ['Lengua', lengua], ['Historia', historia], ['TIC II', tic]]
-media = (mates + lengua + historia + tic) / 4
-print(notas)
-print('Calificación media obtenida:', media)
